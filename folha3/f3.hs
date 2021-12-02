d02aa8800f0a6379eda45c26d95093b876184e7d
Margarida Vieira <margaridajcv.27@gmail.com>
2021-12-02 11:01:11 +0000

    removed unecessary condition

--- folha3/f3.hs
+++ folha3/f3.hs
@@ -19,7 +19,6 @@ dec2Int l = foldl ((+).(*10)) 0 l
 
 --------------------------------------- ex3
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] [] = []
 zipWith' _ _ [] = []
 zipWith' _ [] _ = []
 zipWith' f (x:xs) (y:ys) = (f x y) : (zipWith' f xs ys)
